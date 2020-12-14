2da80121220f82f6316e516d5f24ec4961c93f00
Joe-noh <goflb.jh@gmail.com>
2020-12-15 00:31:17 +0900

    generate mix project
    mix new . --app sptfy --sup

---
+++ lib/sptfy.ex
@@ -0,0 +1,18 @@
+defmodule Sptfy do
+  @moduledoc """
+  Documentation for `Sptfy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sptfy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sptfy_test.exs
@@ -0,0 +1,8 @@
+defmodule SptfyTest do
+  use ExUnit.Case
+  doctest Sptfy
+
+  test "greets the world" do
+    assert Sptfy.hello() == :world
+  end
+end
