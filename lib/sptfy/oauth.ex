8ee6a71549a19e2bd04c3fd826fce90691e80790
Joe-noh <goflb.jh@gmail.com>
2020-12-15 02:48:43 +0900

    get oauth code and exchange it with access tokens

---
+++ lib/sptfy/oauth.ex
@@ -0,0 +1,39 @@
+defmodule Sptfy.OAuth do
+  def url(params) when is_list(params) do
+    params |> Enum.into(%{}) |> url()
+  end
+
+  def url(params) do
+    scope = Map.get(params, :scope, []) |> Enum.join(",")
+
+    endpoint = "https://accounts.spotify.com/authorize"
+    query = Map.merge(params, %{response_type: "code", scope: scope})
+
+    endpoint <> "?" <> URI.encode_query(query)
+  end
+
+  def get_token(params) when is_list(params) do
+    params |> Enum.into(%{}) |> get_token()
+  end
+
+  def get_token(params) do
+    endpoint = "https://accounts.spotify.com/api/token"
+    {client_id, params} = Map.pop(params, :client_id)
+    {client_secret, params} = Map.pop(params, :client_secret)
+    body = Map.merge(params, %{grant_type: "authorization_code"}) |> URI.encode_query()
+
+    Finch.build(:post, endpoint, headers(client_id, client_secret), body)
+    |> Finch.request(Sptfy.Finch)
+    |> case do
+      {:ok, %{body: body}} -> Jason.decode(body)
+      error -> error
+    end
+  end
+
+  defp headers(client_id, client_secret) do
+    [
+      {"Authorization", "Basic " <> Base.encode64(client_id <> ":" <> client_secret)},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+  end
+end
---
+++ test/sptfy/oauth_test.exs
@@ -0,0 +1,54 @@
+defmodule Sptfy.OAuthTest do
+  use ExUnit.Case, async: true
+
+  describe "url/1" do
+    setup do
+      params = [
+        client_id: "CLIENT_ID",
+        redirect_uri: "http://localhost:4000/spotify/callback",
+        scope: ~w[SCOPE1 SCOPE2],
+        state: "STATE",
+        show_dialog: true
+      ]
+
+      %{params: params}
+    end
+
+    test "returns authorize URL", %{params: params} do
+      uri = Sptfy.OAuth.url(params) |> IO.inspect() |> URI.parse()
+
+      assert uri.scheme == "https"
+      assert uri.host == "accounts.spotify.com"
+      assert uri.path == "/authorize"
+
+      expected_query = %{
+        "client_id" => "CLIENT_ID",
+        "redirect_uri" => "https://redirect.uri/callback",
+        "response_type" => "code",
+        "scope" => "SCOPE1,SCOPE2",
+        "show_dialog" => "true",
+        "state" => "STATE"
+      }
+
+      assert expected_query == URI.decode_query(uri.query)
+    end
+
+    test "accept params as a map", %{params: params} do
+      map_params = params |> Enum.into(%{})
+
+      assert Sptfy.OAuth.url(params) == Sptfy.OAuth.url(map_params)
+    end
+  end
+
+  describe "get_token/1" do
+    @tag :skip
+    test "exchange code with tokens" do
+      Sptfy.OAuth.get_token(%{
+        client_id: "CLIENT_ID",
+        client_secret: "CLIENT_SECRET",
+        redirect_uri: "https://redirect.uri/callback",
+        code: "CODE"
+      })
+    end
+  end
+end
