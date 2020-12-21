bd830675563d5744d0bf0fb9339556781ade97e8
Joe-noh <goflb.jh@gmail.com>
2020-12-22 02:46:32 +0900

    assert Spotify API returns a track as expected

--- integration_test/track_test.exs
+++ integration_test/track_test.exs
@@ -2,12 +2,13 @@ defmodule IntegrationTest.TrackTest do
   use ExUnit.Case
 
   alias Sptfy.Track
+  alias Sptfy.Object.FullTrack
 
   setup_all do
     %{token: System.fetch_env!("SPOTIFY_TOKEN")}
   end
 
   test "get_track/2", %{token: token} do
-    Track.get_track(token, id: "3n3Ppam7vgaVa1iaRUc9Lp") |> IO.inspect
+    assert {:ok, %FullTrack{}} = Track.get_track(token, id: "3n3Ppam7vgaVa1iaRUc9Lp")
   end
 end
