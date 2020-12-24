185900ef2cf4b48716cbc633fdd4a504243fa7bb
Joe-noh <goflb.jh@gmail.com>
2020-12-25 00:49:37 +0900

    prefix Simplified

--- test/sptfy/client/body_mapper_test.exs
+++ test/sptfy/client/body_mapper_test.exs
@@ -2,25 +2,25 @@ defmodule Sptfy.Client.BodyMapperTest do
   use ExUnit.Case, async: true
 
   alias Sptfy.Client.BodyMapper
-  alias Sptfy.Object.Artist
+  alias Sptfy.Object.SimplifiedArtist
 
   describe "single/1" do
     test "maps a json object to a struct" do
-      mapping = BodyMapper.single(Artist)
+      mapping = BodyMapper.single(SimplifiedArtist)
 
-      assert BodyMapper.map(%{}, mapping) == %Artist{}
-      assert BodyMapper.map(%{id: 10}, mapping) == %Artist{id: 10}
-      assert BodyMapper.map(%{"id" => 10}, mapping) == %Artist{id: 10}
+      assert BodyMapper.map(%{}, mapping) == %SimplifiedArtist{}
+      assert BodyMapper.map(%{id: 10}, mapping) == %SimplifiedArtist{id: 10}
+      assert BodyMapper.map(%{"id" => 10}, mapping) == %SimplifiedArtist{id: 10}
     end
   end
 
   describe "list_of/2" do
     test "maps a json array to list of structs" do
-      mapping = BodyMapper.list_of(Artist, "key")
+      mapping = BodyMapper.list_of(SimplifiedArtist, "key")
 
-      assert BodyMapper.map(%{"key" => [%{}]}, mapping) == [%Artist{}]
-      assert BodyMapper.map(%{"key" => [%{id: 10}]}, mapping) == [%Artist{id: 10}]
-      assert BodyMapper.map(%{"key" => [%{"id" => 10}]}, mapping) == [%Artist{id: 10}]
+      assert BodyMapper.map(%{"key" => [%{}]}, mapping) == [%SimplifiedArtist{}]
+      assert BodyMapper.map(%{"key" => [%{id: 10}]}, mapping) == [%SimplifiedArtist{id: 10}]
+      assert BodyMapper.map(%{"key" => [%{"id" => 10}]}, mapping) == [%SimplifiedArtist{id: 10}]
     end
   end
 
