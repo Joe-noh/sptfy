4b138f71154fbde0501b64a56514bfdd92504987
Joe-noh <goflb.jh@gmail.com>
2020-12-26 23:07:39 +0900

    define episode related structs

---
+++ lib/sptfy/object/full_episode.ex
@@ -0,0 +1,39 @@
+
+defmodule Sptfy.Object.FullEpisode do
+  use Sptfy.Object
+
+  alias Sptfy.Object.{Image, ResumePoint, SimplifiedShow}
+
+  defstruct ~w[
+    audio_preview_url
+    description
+    duration_ms
+    explicit
+    external_urls
+    href
+    id
+    images
+    is_externally_hosted
+    is_playable
+    language
+    languages
+    name
+    release_date
+    release_date_precision
+    resume_point
+    show
+    type
+    uri
+  ]a
+
+  def new(fields) do
+    fields =
+      fields
+      |> Helpers.atomize_keys()
+      |> Map.update(:images, [], fn images -> Enum.map(images, &Image.new/1) end)
+      |> Map.update(:resume_point, nil, &ResumePoint.new/1)
+      |> Map.update(:show, nil, &SimplifiedShow.new/1)
+
+    struct(__MODULE__, fields)
+  end
+end
---
+++ lib/sptfy/object/resume_point.ex
@@ -0,0 +1,8 @@
+defmodule Sptfy.Object.ResumePoint do
+  use Sptfy.Object
+
+  defstruct ~w[
+    fully_played
+    resume_position_ms
+  ]a
+end
