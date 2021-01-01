49080920afb440f5a4f48e0fe7bffdee575b1d09
Joe-noh <goflb.jh@gmail.com>
2021-01-01 23:32:44 +0900

    make BodyMapper.map be able to return :ok

--- lib/sptfy/client/response_handler.ex
+++ lib/sptfy/client/response_handler.ex
@@ -4,19 +4,19 @@ defmodule Sptfy.Client.ResponseHandler do
   alias Sptfy.Client.BodyMapper
   alias Sptfy.Object.Error
 
-  @spec handle(response :: %Finch.Response{}, mapping :: BodyMapper.t()) :: {:ok, map() | [map()]} | {:error, Error.t()}
+  @spec handle(response :: %Finch.Response{}, mapping :: BodyMapper.t()) :: :ok | {:ok, map()} | {:ok, [map()]} | {:error, Error.t()}
   def handle(%Finch.Response{body: body}, mapping) do
     body |> Jason.decode!() |> handle_json(mapping)
   end
 
   defp handle_json(json, mapping) when is_map(json) do
     case Map.get(json, "error") do
-      nil -> {:ok, Sptfy.Client.BodyMapper.map(json, mapping)}
+      nil -> Sptfy.Client.BodyMapper.map(json, mapping)
       error -> {:error, Sptfy.Object.Error.new(error)}
     end
   end
 
   defp handle_json(json, mapping) when is_list(json) do
-    {:ok, Sptfy.Client.BodyMapper.map(json, mapping)}
+    Sptfy.Client.BodyMapper.map(json, mapping)
   end
 end
--- test/sptfy/client/body_mapper_test.exs
+++ test/sptfy/client/body_mapper_test.exs
@@ -8,9 +8,9 @@ defmodule Sptfy.Client.BodyMapperTest do
     test "maps a json object to a struct" do
       mapping = BodyMapper.single(SimplifiedArtist)
 
-      assert BodyMapper.map(%{}, mapping) == %SimplifiedArtist{}
-      assert BodyMapper.map(%{id: 10}, mapping) == %SimplifiedArtist{id: 10}
-      assert BodyMapper.map(%{"id" => 10}, mapping) == %SimplifiedArtist{id: 10}
+      assert BodyMapper.map(%{}, mapping) == {:ok, %SimplifiedArtist{}}
+      assert BodyMapper.map(%{id: 10}, mapping) == {:ok, %SimplifiedArtist{id: 10}}
+      assert BodyMapper.map(%{"id" => 10}, mapping) == {:ok, %SimplifiedArtist{id: 10}}
     end
   end
 
@@ -18,21 +18,21 @@ defmodule Sptfy.Client.BodyMapperTest do
     test "maps a json array to list of structs" do
       mapping = BodyMapper.list_of(SimplifiedArtist, "key")
 
-      assert BodyMapper.map(%{"key" => [%{}]}, mapping) == [%SimplifiedArtist{}]
-      assert BodyMapper.map(%{"key" => [%{id: 10}]}, mapping) == [%SimplifiedArtist{id: 10}]
-      assert BodyMapper.map(%{"key" => [%{"id" => 10}]}, mapping) == [%SimplifiedArtist{id: 10}]
+      assert BodyMapper.map(%{"key" => [%{}]}, mapping) == {:ok, [%SimplifiedArtist{}]}
+      assert BodyMapper.map(%{"key" => [%{id: 10}]}, mapping) == {:ok, [%SimplifiedArtist{id: 10}]}
+      assert BodyMapper.map(%{"key" => [%{"id" => 10}]}, mapping) == {:ok, [%SimplifiedArtist{id: 10}]}
     end
 
     test "handle list when key is nil" do
       mapping = BodyMapper.list_of(SimplifiedArtist)
 
-      assert BodyMapper.map([nil, %{id: 10}, nil], mapping) == [nil, %SimplifiedArtist{id: 10}, nil]
+      assert BodyMapper.map([nil, %{id: 10}, nil], mapping) == {:ok, [nil, %SimplifiedArtist{id: 10}, nil]}
     end
 
     test "maps nil into nil" do
       mapping = BodyMapper.list_of(SimplifiedArtist, "key")
 
-      assert BodyMapper.map(%{"key" => [nil, %{id: 10}, nil]}, mapping) == [nil, %SimplifiedArtist{id: 10}, nil]
+      assert BodyMapper.map(%{"key" => [nil, %{id: 10}, nil]}, mapping) == {:ok, [nil, %SimplifiedArtist{id: 10}, nil]}
     end
   end
 
@@ -40,9 +40,9 @@ defmodule Sptfy.Client.BodyMapperTest do
     test "maps a json object to structs wrapped by Paging" do
       mapping = BodyMapper.paged(SimplifiedArtist)
 
-      assert BodyMapper.map(%{"items" => []}, mapping) == %Paging{items: []}
-      assert BodyMapper.map(%{"items" => [%{id: 10}]}, mapping) == %Paging{items: [%SimplifiedArtist{id: 10}]}
-      assert BodyMapper.map(%{"items" => [%{"id" => 10}]}, mapping) == %Paging{items: [%SimplifiedArtist{id: 10}]}
+      assert BodyMapper.map(%{"items" => []}, mapping) == {:ok, %Paging{items: []}}
+      assert BodyMapper.map(%{"items" => [%{id: 10}]}, mapping) == {:ok, %Paging{items: [%SimplifiedArtist{id: 10}]}}
+      assert BodyMapper.map(%{"items" => [%{"id" => 10}]}, mapping) == {:ok, %Paging{items: [%SimplifiedArtist{id: 10}]}}
     end
   end
 
@@ -50,9 +50,9 @@ defmodule Sptfy.Client.BodyMapperTest do
     test "returns given map" do
       mapping = BodyMapper.as_is()
 
-      assert BodyMapper.map(%{}, mapping) == %{}
-      assert BodyMapper.map(%{id: 10}, mapping) == %{id: 10}
-      assert BodyMapper.map(%{"id" => 10}, mapping) == %{"id" => 10}
+      assert BodyMapper.map(%{}, mapping) == {:ok, %{}}
+      assert BodyMapper.map(%{id: 10}, mapping) == {:ok, %{id: 10}}
+      assert BodyMapper.map(%{"id" => 10}, mapping) == {:ok, %{"id" => 10}}
     end
   end
 end
