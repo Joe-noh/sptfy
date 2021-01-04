84fb1cb7a663e235886a646a69e411e30080efa6
Joe-noh <goflb.jh@gmail.com>
2021-01-04 15:39:15 +0900

    add put function for base64 encoded jpeg

--- lib/sptfy/client.ex
+++ lib/sptfy/client.ex
@@ -93,4 +93,32 @@ defmodule Sptfy.Client do
       end
     end
   end
+
+  defmacro put_jpeg(path, as: fun, query: query, mapping: mapping, return_type: return_type) do
+    placeholders = Sptfy.Client.Placeholder.extract(path)
+
+    quote location: :keep do
+      @doc """
+      PUT #{unquote(path)}
+      """
+
+      @spec unquote(fun)(token :: String.t(), body :: binary(), params :: Sptfy.Client.params()) :: unquote(return_type)
+      def unquote(fun)(token, body, params \\ %{})
+
+      def unquote(fun)(token, body, params) when is_list(params) do
+        unquote(fun)(token, body, Enum.into(params, %{}))
+      end
+
+      def unquote(fun)(token, body, params) when is_map(params) do
+        query_params = params |> Map.take(unquote(query))
+        path_params = params |> Map.take(unquote(placeholders))
+        filled_path = Sptfy.Client.Placeholder.fill(unquote(path), path_params)
+
+        case Sptfy.Client.HTTP.put_jpeg(token, filled_path, query_params, body) do
+          {:ok, response} -> Sptfy.Client.ResponseHandler.handle(response, unquote(mapping))
+          error -> error
+        end
+      end
+    end
+  end
 end
