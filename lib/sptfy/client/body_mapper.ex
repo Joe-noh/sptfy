3ea9a1a05a5a8af21811b6a11f4adeb1caafdfb5
Joe-noh <goflb.jh@gmail.com>
2021-01-19 18:57:26 +0900

    remove unused function clauses

--- lib/sptfy/client/body_mapper.ex
+++ lib/sptfy/client/body_mapper.ex
@@ -9,14 +9,6 @@ defmodule Sptfy.Client.BodyMapper do
   ]a
 
   @spec map(json :: map() | list(map()), mapping :: t()) :: :ok | {:ok, map()} | {:ok, [map()]} | {:ok, map(), String.t()}
-  def map(json, %__MODULE__{fun: fun, key: nil}) do
-    fun.(json)
-  end
-
-  def map(json, %__MODULE__{fun: fun, key: key}) do
-    Map.get(json, key) |> fun.()
-  end
-
   def map(json, {:single, module: module}) do
     {:ok, module.new(json)}
   end
@@ -68,50 +60,5 @@ defmodule Sptfy.Client.BodyMapper do
 
   def map(_json, :ok) do
     :ok
-  end
-
-  defp do_single(module) do
-    fn
-      nil -> nil
-      fields -> module.new(fields)
-    end
-  end
-
-  @spec list_of(module :: module(), key :: String.t() | nil) :: t()
-  def list_of(module, key \\ nil) do
-    single_fun = do_single(module)
-    %__MODULE__{fun: fn list -> {:ok, Enum.map(list, single_fun)} end, key: key}
-  end
-
-  @spec paged(module :: module(), key :: String.t() | nil) :: t()
-  def paged(module, key \\ nil) do
-    %__MODULE__{fun: fn fields -> {:ok, Sptfy.Object.Paging.new(fields, module)} end, key: key}
-  end
-
-  @spec paged_with_message(module :: module(), key :: String.t() | nil) :: t()
-  def paged_with_message(module, key \\ nil) do
-    fun = fn fields ->
-      %{"message" => message, ^key => pagign_fields} = fields
-      paging = Sptfy.Object.Paging.new(pagign_fields, module)
-
-      {:ok, paging, message}
-    end
-
-    %__MODULE__{fun: fun, key: nil}
-  end
-
-  @spec cursor_paged(module :: module()) :: t()
-  def cursor_paged(module) do
-    %__MODULE__{fun: fn fields -> {:ok, Sptfy.Object.CursorPaging.new(fields, module)} end}
-  end
-
-  @spec as_is(key :: String.t() | nil) :: t()
-  def as_is(key \\ nil) do
-    %__MODULE__{fun: fn fields -> {:ok, fields} end, key: key}
-  end
-
-  @spec ok :: t()
-  def ok do
-    %__MODULE__{fun: fn _ -> :ok end}
   end
 end
--- lib/sptfy/client/return_type.ex
+++ lib/sptfy/client/return_type.ex
@@ -2,17 +2,9 @@ defmodule Sptfy.Client.ReturnType do
   @moduledoc false
 
   @doc false
-  @spec ast(tuple()) :: tuple() | :ok | nil
-  def ast({:single, _meta, [module | _]}) do
-    ok(t(module))
-  end
-
+  @spec ast(type :: tuple() | atom()) :: tuple() | :ok | nil
   def ast({:single, module: module}) do
     ok(t(module))
-  end
-
-  def ast({:list_of, _meta, [module | _]}) do
-    ok([t(module)])
   end
 
   def ast({:list, opts}) do
@@ -21,32 +13,16 @@ defmodule Sptfy.Client.ReturnType do
     ok([t(module)])
   end
 
-  def ast({:paged, _meta, _args}) do
-    ok(t(Sptfy.Object.Paging))
-  end
-
   def ast({:paging, _opts}) do
     ok(t(Sptfy.Object.Paging))
-  end
-
-  def ast({:paged_with_message, _meta, _args}) do
-    {:{}, [], [:ok, t(Sptfy.Object.Paging), t(String)]}
   end
 
   def ast({:paging_with_message, _opts}) do
     {:{}, [], [:ok, t(Sptfy.Object.Paging), t(String)]}
   end
 
-  def ast({:cursor_paged, _meta, _args}) do
-    ok(t(Sptfy.Object.CursorPaging))
-  end
-
   def ast({:cursor_paging, _opts}) do
     ok(t(Sptfy.Object.CursorPaging))
-  end
-
-  def ast({:as_is, _meta, _args}) do
-    nil
   end
 
   def ast(:as_is) do
