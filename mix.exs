d96e6e2a974e63b59f42d83db8893c8dde49e426
Joe-noh <goflb.jh@gmail.com>
2023-03-19 01:21:16 +0900

    v0.1.5
    Add support for players queue API

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Sptfy.MixProject do
       name: "Sptfy",
       description: "Spotify API client library.",
       app: :sptfy,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
